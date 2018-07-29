0f0f7e4e4db7ef8b97a339f2207196939033d484
bjsmith3185 <bjsmith3185@hotmail.com>
2018-07-29 17:00:24 -0400

    working, gonna add reset button

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,6 @@
 
 $(document).ready(function(){
 
-
-
-
-
-
 var wweArray = [
     {
         image: "assets/images/chris.jpg",
@@ -17,60 +12,70 @@ var wweArray = [
         image: "assets/images/edge.jpg",
         name: "Edge ",
         choices: ["Mick Foley","Vince McMahon", "Jeff Hardy", "Edge "],
+        transition: "assets/images/transitions/chris_smile.gif",
     },
     {
         image: "assets/images/booker.png",
         name: "Booker T",
         choices: ["Daniel Bryan","Kane", "Undertaker", "Booker T"],
+        transition: "assets/images/transitions/christian_Slap.gif",
     },
     {
         image: "assets/images/hhh.png",
         name: "HHH",
         choices: ["Kurt Angle","Brock Lesnar", "Big Show", "HHH"],
+        transition: "assets/images/transitions/double_slam.gif",
     },
     {
         image: "assets/images/irs.jpg",
         name: "IRS",
         choices: ["Kane","The Miz", "Shawn Michaels", "IRS"],
+        transition: "assets/images/transitions/hhh_dx.gif",
     },
     {
         image: "assets/images/matt.jpg",
         name: "Matt Hardy",
         choices: ["Undertaker","Daniel Bryan", "Jeff Hardy", "Matt Hardy"],
+        transition: "assets/images/transitions/irs.gif",
     },
     {
         image: "assets/images/randy.jpg",
         name: "Randy Orton",
         choices: ["Shawn Michaels","Big Show", "Dolph Ziggler", "Randy Orton"],
+        transition: "assets/images/transitions/people_eyebrow.gif",
     },
     {
         image: "assets/images/ric.jpg",
         name: "Ric Flair",
         choices: ["Jeff Hardy","Undertaker", "John Cena", "Ric Flair"],
+        transition: "assets/images/transitions/steve_austin.gif",
     },
     {
         image: "assets/images/rock.jpg",
         name: "The Rock",
         choices: ["Big Show","Kurt Angle", "John Cena", "The Rock"],
+        transition: "assets/images/transitions/hhh_pedigree.gif",
     },
     {
         image: "assets/images/sheamus.jpg",
         name: "Sheamus",
         choices: ["Kane","Brock Lesnar", "Shawn Michaels", "Sheamus"],
+        transition: "assets/images/transitions/Scoop_Slam.gif",
     },
     {
         image: "assets/images/steve.jpg",
         name: "Steve Austin",
         choices: ["Undertaker","John Cena", "AJ Styles", "Steve Austin"],
+        transition: "assets/images/transitions/rock_bring_it.gif",
     },
 ];
-
 
 var timesUp = true;
 var index = 0;
 var timerRunning;
 var tempGameArray = [];
 var selected;
+var endOfGame = false;
 
 // end of variables
 
@@ -81,37 +86,26 @@ $(".correct-answer").hide();
 $(".results").hide();
 $("#picture").attr("src", wweArray[0].image)
 
-
-
-
-
-
+// welcome start button 
 
 $(".start").on("click", function() {
     displayQuestion();
     $(".welcome").hide();
     $(".game").show();
-
-
-
 });
+
+// select answer button 
 
 $(".show-choices").on("click", ".name", function() {
     selected = $(this).attr("data-name");
-    console.log("this is selected: " + selected);
     timesUp = false;
-    
     clearTimeout(timerRunning);
     checkname();
-   
-    // $(".game").hide();
-    // $(".correct-answer").show();
 });
 
 function checkname() {
     $(".game").hide();
     $(".correct-answer").show();
-
 
     var arrayLocation = wweArray[index];
     var objectForArray = new Object();
@@ -127,63 +121,43 @@ function checkname() {
         $(".correct-title").text("Wrong..");
 
     } else {
-       
         tempGameArray[index].username = selected;
 
-        console.log("selected: " + tempGameArray[index].username + "  correct: " +  tempGameArray[index].name);
-       
         if ( tempGameArray[index].username === tempGameArray[index].name) {
-            console.log("you are correct");
             $(".correct-title").text("Correct!!");
             $(".show-name").text(wweArray[index].name);
         } else {
-           console.log("you are incorrect");
-           console.log(timesUp);
            $(".correct-title").text("Wrong..");
            $(".show-name").text(wweArray[index].name);
-           
         };
     };
 
-    // index++;
-    // timesUp = true;
-
-    if ( index === wweArray.length) {
-        gradeQuiz();
-        // $(".results-screen").show();
+    if ( (index + 1) === wweArray.length) {
+        endOfGame = true;
+        $(".correct-answer").show();
+        $(".show-image-again").attr("src", wweArray[index].image);
     } else {
-        // displayCorrect();
         $(".game").hide();
         $(".correct-answer").show();
-        
         $(".show-image-again").attr("src", wweArray[index].image);
+    };
 
-       //--------------------------
+    if (endOfGame) {
+        setTimeout(gradeQuiz, 3000);
+    } else {
         setTimeout(transition, 3000);
-
-       
     };
 
     index++;
     timesUp = true;
 };
 
-
-function displayCorrect() {
-    $(".game").hide();
-    $(".correct-answer").show();
-    
-    $(".show-image-again").text(wweArray[index-1].image);
-}
-
-
 function transition() {
-   
     $(".transition").show();
     $(".correct-answer").hide();
     $(".transition-title").text("Are you ready bro?");
-    $(".transition-image").attr("src", wweArray[0].transition );
-    setTimeout(displayQuestion, 1900)
+    $(".transition-image").attr("src", wweArray[index-1].transition );
+    setTimeout(displayQuestion, 2000)
 
 };
 
@@ -192,43 +166,30 @@ function displayQuestion() {
     $(".transition").hide();
     $(".game").show();
 
-    // shows the question in the dom
-
     var randomAnswersArray = [];
     $(".show-name").empty();
     $(".show-image").empty();
     $(".show-choices").empty();
-    // $(".show-image").html("<img src='" + wweArray[index].image + "'>").addClass("");
     $(".show-image").attr("src", wweArray[index].image)
-    // console.log("this is the name: " + wweArray[index].name);
-
+    console.log("this is the answer: " + wweArray[index].name);
 
     // shuffles the answers array and displays in the dom 
-
     for (var i = 0; i < 4; i++) {
         randomAnswersArray.push(wweArray[index].choices[i]);
     };
-    console.log("this is randomnameArray:");
-    console.log(randomAnswersArray);
+
     randomAnswersArray.sort(function (a, b) { return 0.5 - Math.random() });
-    console.log("this is randomnameArray after sorting:")
-    console.log(randomAnswersArray);
 
     for (var i = 0; i <randomAnswersArray.length; i++) {
-        // console.log(wweArray[i].choices[i]);
         var newDiv = $("<div>");
         newDiv.text(randomAnswersArray[i]).addClass("name").attr("data-name", randomAnswersArray[i]);
         $(".show-choices").append(newDiv);
-        // $(".show-choices").append(randomAnswersArray[i]);
-     };
-
+    };
 
     timerRunning = setTimeout(checkname, 2 * 5000);
-
 };
 
 function gradeQuiz() {
-    $(".transition").hide();
     $(".correct-answer").hide();
     $(".results").show();
 
@@ -241,26 +202,22 @@ function gradeQuiz() {
         } else {
             incorrect++;
         };
-
-    }
+    };
 
     if (correct === 0) {
-        // display something saying grade = 0;
-        alert('Hi')
-        $(".grade").text("your grade is 0%, maybe try harder?");
+        $(".grade").text("You scored 0%, Maybe try harder?").addClass("loser");
+
+    } else if (correct === tempGameArray.length) {
+        $(".grade").text("You got em all right bro!");
 
     } else {
         var grade = (incorrect/tempGameArray.length) * 100;
 
-        $(".grade").text("your grade is: " + grade);
-        $(".correct").text("You got " + correct + " correct!");
-        $(".incorrect").text("You missed " + incorrect + ".");
-        
-        };
-       
-
-    
+        $(".grade").text("You Scored " + grade +"%");
+        $(".correct").text(correct);
+        $(".incorrect").text(incorrect);
+    };
 };
 
 }); // end of document ready
-//---------------------------------------------------------------------------
+

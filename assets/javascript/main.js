b9dbeb14702cd9516a92341a1c99148fa7fbe417
bjsmith3185 <bjsmith3185@hotmail.com>
2018-07-29 15:33:13 -0400

    adding css to screens

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,36 +2,66 @@
 $(document).ready(function(){
 
 
+
+
+
+
 var wweArray = [
     {
-        image: "image 0",
-        name: "name 0",
-        choices: ["answer 0","answer 1", "answer 2", "name 0"],
+        image: "assets/images/chris.jpg",
+        name: "Chris Jericho",
+        choices: ["AJ Styles","Undertaker", "Daniel Bryan", "Chris Jericho"],
+        transition: "assets/images/transitions/ladder_transition.gif",
     },
     {
-        image: "image 1",
-        name: "name 1",
-        choices: ["answer 0","answer 1", "answer 2", "name 1"],
+        image: "assets/images/edge.jpg",
+        name: "Edge ",
+        choices: ["Mick Foley","Vince McMahon", "Jeff Hardy", "Edge "],
     },
     {
-        image: "image 2",
-        name: "name 2",
-        choices: ["answer 0","answer 1", "answer 2", "name 2"],
+        image: "assets/images/booker.png",
+        name: "Booker T",
+        choices: ["Daniel Bryan","Kane", "Undertaker", "Booker T"],
     },
     {
-        image: "image 3",
-        name: "name 3",
-        choices: ["answer 0","answer 1", "answer 2", "name 3"],
+        image: "assets/images/hhh.png",
+        name: "HHH",
+        choices: ["Kurt Angle","Brock Lesnar", "Big Show", "HHH"],
     },
     {
-        image: "image 4",
-        name: "name 4",
-        choices: ["answer 0","answer 1", "answer 2", "name 4"],
+        image: "assets/images/irs.jpg",
+        name: "IRS",
+        choices: ["Kane","The Miz", "Shawn Michaels", "IRS"],
     },
     {
-        image: "image 5",
-        name: "name 5",
-        choices: ["answer 0","answer 1", "answer 2", "name 5"],
+        image: "assets/images/matt.jpg",
+        name: "Matt Hardy",
+        choices: ["Undertaker","Daniel Bryan", "Jeff Hardy", "Matt Hardy"],
+    },
+    {
+        image: "assets/images/randy.jpg",
+        name: "Randy Orton",
+        choices: ["Shawn Michaels","Big Show", "Dolph Ziggler", "Randy Orton"],
+    },
+    {
+        image: "assets/images/ric.jpg",
+        name: "Ric Flair",
+        choices: ["Jeff Hardy","Undertaker", "John Cena", "Ric Flair"],
+    },
+    {
+        image: "assets/images/rock.jpg",
+        name: "The Rock",
+        choices: ["Big Show","Kurt Angle", "John Cena", "The Rock"],
+    },
+    {
+        image: "assets/images/sheamus.jpg",
+        name: "Sheamus",
+        choices: ["Kane","Brock Lesnar", "Shawn Michaels", "Sheamus"],
+    },
+    {
+        image: "assets/images/steve.jpg",
+        name: "Steve Austin",
+        choices: ["Undertaker","John Cena", "AJ Styles", "Steve Austin"],
     },
 ];
 
@@ -49,6 +79,7 @@ $(".game").hide();
 $(".transition").hide();
 $(".correct-answer").hide();
 $(".results").hide();
+$("#picture").attr("src", wweArray[0].image)
 
 
 
@@ -68,9 +99,11 @@ $(".start").on("click", function() {
 $(".show-choices").on("click", ".name", function() {
     selected = $(this).attr("data-name");
     console.log("this is selected: " + selected);
+    timesUp = false;
+    
     clearTimeout(timerRunning);
     checkname();
-    timesUp = false;
+   
     // $(".game").hide();
     // $(".correct-answer").show();
 });
@@ -91,36 +124,48 @@ function checkname() {
         // if timesUp is true then the time ran out and they missed the question
         tempGameArray[index].username = "incorrect";
         $(".show-name").text(wweArray[index].name);
-        console.log("this is under the timer, shows the correct: " + wweArray[index].name);
+        $(".correct-title").text("Wrong..");
 
     } else {
        
         tempGameArray[index].username = selected;
+
+        console.log("selected: " + tempGameArray[index].username + "  correct: " +  tempGameArray[index].name);
        
         if ( tempGameArray[index].username === tempGameArray[index].name) {
             console.log("you are correct");
-            $(".show-name").text("correct");
+            $(".correct-title").text("Correct!!");
+            $(".show-name").text(wweArray[index].name);
         } else {
            console.log("you are incorrect");
+           console.log(timesUp);
+           $(".correct-title").text("Wrong..");
            $(".show-name").text(wweArray[index].name);
            
         };
     };
 
-    index++;
-    timesUp = true;
+    // index++;
+    // timesUp = true;
 
     if ( index === wweArray.length) {
         gradeQuiz();
         // $(".results-screen").show();
     } else {
-        displayCorrect();
-        // transition()
-        // setTimeout(displayQuestion, 1000)  this was in the original
-        setTimeout(transition, 2000);
+        // displayCorrect();
+        $(".game").hide();
+        $(".correct-answer").show();
+        
+        $(".show-image-again").attr("src", wweArray[index].image);
+
+       //--------------------------
+        setTimeout(transition, 3000);
 
        
     };
+
+    index++;
+    timesUp = true;
 };
 
 
@@ -136,8 +181,9 @@ function transition() {
    
     $(".transition").show();
     $(".correct-answer").hide();
-    $(".background-color").text("Are you ready?");
-    setTimeout(displayQuestion, 1000)
+    $(".transition-title").text("Are you ready bro?");
+    $(".transition-image").attr("src", wweArray[0].transition );
+    setTimeout(displayQuestion, 1900)
 
 };
 
@@ -153,14 +199,14 @@ function displayQuestion() {
     $(".show-image").empty();
     $(".show-choices").empty();
     // $(".show-image").html("<img src='" + wweArray[index].image + "'>").addClass("");
-    $(".show-image").text(wweArray[index].image)
-    console.log("this is the name: " + wweArray[index].name);
+    $(".show-image").attr("src", wweArray[index].image)
+    // console.log("this is the name: " + wweArray[index].name);
 
 
     // shuffles the answers array and displays in the dom 
 
     for (var i = 0; i < 4; i++) {
-        randomAnswersArray.push(wweArray[i].choices[i]);
+        randomAnswersArray.push(wweArray[index].choices[i]);
     };
     console.log("this is randomnameArray:");
     console.log(randomAnswersArray);
@@ -177,7 +223,7 @@ function displayQuestion() {
      };
 
 
-    timerRunning = setTimeout(checkname, 2 * 3000);
+    timerRunning = setTimeout(checkname, 2 * 5000);
 
 };
 
